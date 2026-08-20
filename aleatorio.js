0f05f6f682f2b44a45d810d2b031466dee4801e3
Antonio-Carlos214 <antonio.zamilian@escola.pr.gov.br>
2026-08-20 08:24:08 -0300

    gvftdxyfcgvjh

--- aleatorio.js
+++ aleatorio.js
@@ -1,7 +1,20 @@
-const nomes = ["Fernanda", "Giuliana", "Maria Eduarda", "Marcelo", "Amanda","Gustavo", "Gabriel"];
+const nomes = [
+    "Fernanda",
+    "Giuliana",
+    "Maria Eduarda",
+    "Marcelo",
+    "Amanda",
+    "Gustavo",
+    "Gabriel"
+];
 
-export function aleatorio (lista){
-    const posicao = Math.floor(Math.random()* lista.length);
+export function aleatorio(lista) {
+    if (!lista || lista.length === 0) {
+        return "";
+    }
+
+    const posicao = Math.floor(Math.random() * lista.length);
+
     return lista[posicao];
 }
 
